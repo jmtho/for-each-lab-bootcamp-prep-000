2bb6c6eaaa6119d16368e25f857d0cab3d79d32c
Julia Thompson <thompson.julia@gmail.com>
2016-10-31 15:54:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function iterativeLog(array){
+  array.forEach((element,index,array) => {console.log(`${index}: ${element}`)})
+}
+
+function iterate(callback){
+  var newArray = [3,6,8];
+  newArray.forEach(callback);
+  return newArray;
+}
+
+function doToArray(array,callback){
+  array.forEach(callback);
+}
